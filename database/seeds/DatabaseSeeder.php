657644f75fcee79f18ddc87a41d45bf2ed0476cb
Mostafa <mostafa0220@gmail.com>
2021-06-21 06:27:13 +0400

    update database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('videos')->insert([
+            'id' => 18,
+            'title' => 'Earth',
+            'original_name' => 'file_example_MP4_1920_18MG.mp4',
+            'disk' => 'videos_disk',
+            'path' => 'videos/z9CPpRtiTSi5Ye9hr9XmRPX7NLuiewPSANgMaBcY.mp4',
+            'converted_for_downloading_at' => '2021-06-20 17:55:41'
+        ]);
+        DB::table('videos')->insert([
+            'id' => 19,
+            'title' => 'Carton',
+            'original_name' => 'sample-mp4-file.mp4',
+            'disk' => 'videos_disk',
+            'path' => 'videos/JD6ffYklGRqRG6OJ3b2BtSNrY01QmoQFene9MrhO.mp4',
+            'converted_for_downloading_at' => '2021-06-20 17:55:41'
+        ]);
     }
 }
